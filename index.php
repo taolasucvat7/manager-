f023e0225ccbc8d270208619592ba40d5795699f
taolasucvat5 <31107288+taolasucvat5@users.noreply.github.com>
2017-08-17 10:16:20 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,50 @@
 <?php
+// Create a new cURL resource
+        $curl = curl_init(); 
 
-echo 'asd';
+        if (!$curl) {
+            die("Couldn't initialize a cURL handle"); 
+        }
 
+        // Set the file URL to fetch through cURL
+        curl_setopt($curl, CURLOPT_URL, "http://offer.ebay.com/ws/eBayISAPI.dll?ViewBidsLogin&item=132121376526");
+
+        // Set a different user agent string (Googlebot)
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Googlebot/2.1 (+http://www.google.com/bot.html)'); 
+
+        // Follow redirects, if any
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true); 
+
+        // Fail the cURL request if response code = 400 (like 404 errors) 
+        curl_setopt($curl, CURLOPT_FAILONERROR, true); 
+
+        // Return the actual result of the curl result instead of success code
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        // Wait for 10 seconds to connect, set 0 to wait indefinitely
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
+
+        // Execute the cURL request for a maximum of 50 seconds
+        curl_setopt($curl, CURLOPT_TIMEOUT, 50);
+
+        // Do not check the SSL certificates
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); 
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); 
+
+        // Fetch the URL and save the content in $html variable
+        $html = curl_exec($curl); 
+		echo $html;
+        // Checek if any error has occurred 
+        if (curl_errno($curl)) 
+        {
+            echo 'cURL error: ' . curl_error($curl); 
+        } 
+        else 
+        { 
+            // cURL executed successfully
+            print_r(curl_getinfo($curl)); 
+        }
+
+        // close cURL resource to free up system resources
+        curl_close($curl);
 ?>
