e771879a2f656fb61754fb500b94aee106ed2763
taolasucvat5 <31107288+taolasucvat5@users.noreply.github.com>
2018-08-15 00:14:08 +0700

    Create check-ip.php

---
+++ check-ip.php
@@ -0,0 +1,16 @@
+<?php
+$url = 'https://www.myip.com/';
+ 
+// Khởi tạo CURL
+$ch = curl_init($url);
+ 
+// Thiết lập có return
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ 
+$result = curl_exec($ch);
+ 
+curl_close($ch);
+
+file_put_contents("log.txt", $result);
+
+?>
