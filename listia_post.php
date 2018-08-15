bcc5ed8449c5e24d03a6a8d50137f5f537a303a2
taolasucvat5 <31107288+taolasucvat5@users.noreply.github.com>
2018-08-16 00:34:54 +0700

    Update listia_post.php

--- listia_post.php
+++ listia_post.php
@@ -142,7 +142,7 @@ function getInfo($ch){
 			$result["error"] = false;
 			return $result;
 		}
-
+		/*
 		$list_a = $dom->getElementById("sub_menu_your_auctions")->getElementsByTagName("a");
 
 		
@@ -157,7 +157,7 @@ function getInfo($ch){
 		$result["username"] = getElementByClass($dom, "a", "lt-user-login")->nodeValue;
 
 		$result["avatar"] = getElementByClass($dom, "div", "avatar")->getElementsByTagName("img")[0]->getAttribute("src");
-
+		*/
 		
 
 	}catch (Exception $e) {
