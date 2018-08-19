af84497852ee4c19a55642782e909443c455b4a6
taolasucvat5 <31107288+taolasucvat5@users.noreply.github.com>
2018-08-19 22:12:57 +0700

    Update listia_post.php

--- listia_post.php
+++ listia_post.php
@@ -1,4 +1,6 @@
-<?php if($_GET["style"] == "web"){ ?>
+<?php 
+if($_GET["style"] == "web"){ 
+?>
 <script
   src="https://code.jquery.com/jquery-2.2.4.js"
   integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
@@ -22,7 +24,7 @@
 		border-bottom: 1px solid black;
 	}
 </style>
-} ?>
+<?php } ?>
 
 
 
